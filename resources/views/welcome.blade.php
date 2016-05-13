1303d8f47df43636fa8ea913fcd868aa7bf18d2c
Ari-Pekka Koponen <ap.koponen@gmail.com>
2016-05-13 09:45:49 +0300

    Initial commit with Laravel.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>OAuth 2.0 Test Suite</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,100" rel="stylesheet" type="text/css">
+
+  <style>
+    html,
+    body {
+      height: 100%;
+    }
+
+    body {
+      margin: 0;
+      padding: 0;
+      width: 100%;
+      display: table;
+      font-weight: 100;
+      font-family: 'Lato';
+    }
+
+    .container {
+      text-align: center;
+      display: table-cell;
+      vertical-align: middle;
+    }
+
+    .content {
+      text-align: center;
+      display: inline-block;
+    }
+
+    .title {
+      font-weight: 400;
+      font-size: 64px;
+    }
+
+    .description {
+      font-weight: 100;
+      font-size: 32px;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="container">
+    <div class="content">
+      <div class="title">OAuth 2.0 Test Suite</div>
+      <div class="description">
+        <p>Welcome to testing your OAuth 2.0 implementation.</p>
+        <p>Server: /server/json/</p>
+        <p>Client: /client/json/</p>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
